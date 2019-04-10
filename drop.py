e32cb82bfe22bdde065aab5b7fe71ba33531b61f
Alex Berman <alexberman999@gmail.com>
2019-04-10 16:22:29 -0400

    Added some documentation

--- drop.py
+++ drop.py
@@ -9,9 +9,19 @@ M = 0.267
 G = 9.81
 DT = 0.005
 
+"""
+    Calculates the down-range distance in meters
+    of an object described by the above parameters
+    traveling at velocity v (m/s), at height h (m) in
+    an environment with tail wind tail_wind (m/s). This
+    will not account for crosswinds and assumes a constant
+    windspeed. It is further assumed the object is travelling
+    horizontally.
+"""
 def distance_down_range(v, h, tail_wind):
     x = [0]
     y = [h]
+    #This assumes the object is travelling horizontally.
     theta = 0
     vx = v * numpy.cos(theta)
     vy = v * numpy.sin(theta)
@@ -32,6 +42,7 @@ def distance_down_range(v, h, tail_wind):
     return [x, y]
 
 axis = distance_down_range(18, 45, 3)
+#Printing landing point
 print("X: " + str(axis[0][-1]) + "m\tY: " + str(axis[1][-1]) + "m")
 plt.plot(axis[0], axis[1])
 plt.xlabel("Down Range Distance (Meters)")
